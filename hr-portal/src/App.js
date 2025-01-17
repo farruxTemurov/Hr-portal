e319fe3c7cb773a5c07f99cdd710b23df9e5c19e
Farrux Temurov <tfarrux70@gmail.com>
2025-01-17 12:16:15 +0500

    changed navbar and background image

--- hr-portal/src/App.js
+++ hr-portal/src/App.js
@@ -3,18 +3,21 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './Components/Header/header'; // Import Header component
 import SignUp from './Components/Auth/SignUp'; // Import SignUp component
 import Login from './Components/Auth/Login'; // Import Login component
+import './App.css'; // Import the CSS file with background styles
 
 function App() {
   return (
     <Router>
       <div className="App">
-        <Header />
-
-        <Routes>
-          <Route path="/signup" element={<SignUp />} />
-          <Route path="/login" element={<Login />} />
-
-        </Routes>
+        {/* Add a container with the background styling */}
+        <div className="background-container">
+          <Header />
+          <Routes>
+            <Route path="/signup" element={<SignUp />} />
+            <Route path="/login" element={<Login />} />
+            {/* Add other routes here */}
+          </Routes>
+        </div>
       </div>
     </Router>
   );
--- hr-portal/src/Components/Header/header.js
+++ hr-portal/src/Components/Header/header.js
@@ -3,24 +3,11 @@ import { Link } from 'react-router-dom';
 
 function Header() {
     return (
-        <header>
-            <nav className="navbar navbar-expand-lg navbar-light bg-light">
-                <div className="container">
-                    <Link className="navbar-brand font-weight-bold" to="/" style={{ fontSize: '24px' }}>
-                        HR
-                    </Link>
-
-                    <div className="ml-auto">
-                        <ul className="navbar-nav">
-                            <li className="nav-item">
-                                <Link className="nav-link" to="/signup">Sign Up</Link>
-                            </li>
-                            <li className="nav-item">
-                                <Link className="nav-link" to="/login">Login</Link>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
+        <header className="App-header">
+            <div className="logo">HR</div>
+            <nav className="nav-links">
+                <Link to="/signup">SignUp</Link>
+                <Link to="/login">Login</Link>
             </nav>
         </header>
     );
