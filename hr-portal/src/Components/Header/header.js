1ad10196d9e8a1c6109fd3ff8825aecc0c9b5a3c
Farrux Temurov <tfarrux70@gmail.com>
2025-01-19 22:11:23 +0500

    Header logo updated

--- hr-portal/src/Components/Header/header.js
+++ hr-portal/src/Components/Header/header.js
@@ -4,7 +4,9 @@ import { Link } from 'react-router-dom';
 function Header() {
     return (
         <header className="App-header">
-            <div className="logo">HR Portal</div>
+            <Link to="/signup" className="logo">
+                HR Portal
+            </Link>
             <nav className="nav-links">
                 <Link to="/signup">SignUp</Link>
                 <Link to="/login">Login</Link>
